695f9f940ed9f26131632b19ee9b9f9337084341
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-18 17:58:36 -0400

    Added sprint and crouch

---
+++ Assets/Scripts/PlayerScripts/PlayerSprintAndCrouch.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement _playerMovement;
+    private Transform _lookRoot;
+
+    public float SprintSpeed = 10f;
+    public float MoveSpeed = 5f;
+    public float CrouchSpeed = 2f;
+
+    private float _standHeight = 1.6f;
+    private float _crouchHeight = 1f;
+
+    private bool _isCrouching;
+
+    void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+        _lookRoot = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        Sprint();
+        Crouch();
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !_isCrouching)
+        {
+            _playerMovement.Speed = SprintSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !_isCrouching)
+        {
+            _playerMovement.Speed = MoveSpeed;
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (_isCrouching)
+            {
+                _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
+                _playerMovement.Speed = MoveSpeed;
+                _isCrouching = false;
+            }
+            else
+            {
+                _lookRoot.localPosition = new Vector3(0f, _crouchHeight, 0f);
+                _playerMovement.Speed = CrouchSpeed;
+                _isCrouching = true;
+
+            }
+        }
+    }
+}
