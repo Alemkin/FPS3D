28c4d8db91ff5dee1b3b72f01fdba34bf649ef47
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-17 22:46:34 -0400

    Applied code for movement and jump

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private CharacterController _characterController;
+    private Vector3 _moveDirection;
+
+    public float Speed = 5f;
+    public float JumpForce = 10f;
+    public float Gravity = 20f;
+    public float VerticalVelocity;
+   
+    void Awake()
     {
-        
+        _characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        _moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+        _moveDirection = transform.TransformDirection(_moveDirection);
+        _moveDirection = _moveDirection * Speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        _characterController.Move(_moveDirection);
+    }
+
+    void ApplyGravity()
+    {
+        VerticalVelocity -= Gravity * Time.deltaTime;
+        PlayerJump();
+        _moveDirection.y = VerticalVelocity * Time.deltaTime;
+    }
+
+    void PlayerJump()
+    {
+        if (_characterController.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            VerticalVelocity = JumpForce;
+        }
     }
 }
