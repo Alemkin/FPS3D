ef35d651bf8027d483030f57c1f637e17a9012e0
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-24 16:38:04 -0400

    Animated weapions, added weapon manager and handlers to select weapons

---
+++ Assets/Scripts/WeaponsScripts/WeaponHandler.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.WeaponsScripts
+{
+
+    public enum WeaponAim
+    {
+        None,
+        SelfAim,
+        Aim
+    }
+
+    public enum WeaponFireType
+    {
+        Single,
+        Multiple
+    }
+
+    public enum WeaponBulletType
+    {
+        Bullet,
+        Arrow,
+        Spear,
+        None
+    }
+
+    public class WeaponHandler : MonoBehaviour
+    {
+
+        private Animator _animator;
+
+        [SerializeField]
+        private GameObject _muzzleFlash;
+
+        [SerializeField]
+        private AudioSource _shootSound, _reloadSound;
+
+        public WeaponFireType FireType;
+
+        public WeaponBulletType BulletType;
+
+        public GameObject AttackPoint;
+
+        public WeaponAim WeaponAim;
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+
+        }
+
+        public void AttackAnimation()
+        {
+            _animator.SetTrigger(AnimationTags.ATTACK_TRIGGER);
+        }
+
+        public void Aim(bool canAim)
+        {
+            _animator.SetBool(AnimationTags.AIM_PARAMETER, canAim);
+        }
+
+        void TurnOnMuzzleFlash()
+        {
+            _muzzleFlash.SetActive(true);
+        }
+
+        void TurnOffMuzzleFlash()
+        {
+            _muzzleFlash.SetActive(false);
+        }
+
+        void PlayShootSound()
+        {
+            _shootSound.Play();
+        }
+
+        void PlayReloadSound()
+        {
+            _reloadSound.Play();
+        }
+
+        void TurnOnAttackPoint()
+        {
+            AttackPoint.SetActive(true);
+        }
+
+        void TurnOffAttackPoint()
+        {
+            if (AttackPoint.activeInHierarchy)
+            {
+                AttackPoint.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponsScripts/WeaponManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.WeaponsScripts
+{
+    public class WeaponManager : MonoBehaviour
+    {
+        [SerializeField]
+        private WeaponHandler[] _weapons;
+
+        private int _currentWeaponIndex;
+
+        void Start()
+        {
+            _currentWeaponIndex = 0;
+            _weapons[_currentWeaponIndex].gameObject.SetActive(true);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                TurnOnSelectedWeapon(0);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                TurnOnSelectedWeapon(1);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                TurnOnSelectedWeapon(2);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                TurnOnSelectedWeapon(3);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                TurnOnSelectedWeapon(4);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                TurnOnSelectedWeapon(5);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                var nextIndex = _currentWeaponIndex < _weapons.Length - 1 ? _currentWeaponIndex + 1 : 0;
+                TurnOnSelectedWeapon(nextIndex);
+            }
+        }
+
+        void TurnOnSelectedWeapon(int weaponIndex)
+        {
+            if (weaponIndex == _currentWeaponIndex) return;
+            _weapons[_currentWeaponIndex].gameObject.SetActive(false);
+            _weapons[weaponIndex].gameObject.SetActive(true);
+            _currentWeaponIndex = weaponIndex;
+        }
+
+        public WeaponHandler GetCurrentSelectedWeapon()
+        {
+            return _weapons[_currentWeaponIndex];
+        }
+    }
+}
