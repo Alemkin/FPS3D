2b548b48ce3a7acef1512c7a20e54a5bde6d0f32
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-26 15:34:10 -0400

    Updated so you can't crouch mid air and incrased footstep sound distance

--- Assets/Scripts/PlayerScripts/PlayerSprintAndCrouch.cs
+++ Assets/Scripts/PlayerScripts/PlayerSprintAndCrouch.cs
@@ -1,97 +1,99 @@
-﻿using Assets.Scripts.PlayerScripts;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerSprintAndCrouch : MonoBehaviour
+namespace Assets.Scripts.PlayerScripts
 {
-    private PlayerMovement _playerMovement;
-    private Transform _lookRoot;
-
-    public float SprintSpeed = 10f;
-    public float MoveSpeed = 5f;
-    public float CrouchSpeed = 2f;
-
-    private float _standHeight = 1.6f;
-    private float _crouchHeight = 1f;
-
-    private bool _isCrouching;
-
-    private PlayerFootsteps _playerFootsteps;
-    private CharacterController _characterController;
-    private float _sprintVolume = 1f;
-    private float _crouchVolume = 0.1f;
-    private float _walkVolumeMin = 0.2f, _walkVolumeMax = 0.6f;
-    private float _walkStepDistance = 0.4f;
-    private float _sprintStepDistance = 0.25f;
-    private float _crouchStepDistance = 0.5f;
-
-    void Start()
+    public class PlayerSprintAndCrouch : MonoBehaviour
     {
-        _playerFootsteps.VolumeMin = _walkVolumeMin;
-        _playerFootsteps.VolumeMax = _walkVolumeMax;
-        _playerFootsteps.StepDistance = _walkStepDistance;
-    }
+        private PlayerMovement _playerMovement;
+        private Transform _lookRoot;
 
-    void Awake()
-    {
-        _playerMovement = GetComponent<PlayerMovement>();
-        _lookRoot = transform.GetChild(0);
-        _playerFootsteps = GetComponentInChildren<PlayerFootsteps>();
-        _characterController = GetComponent<CharacterController>();
-    }
+        public float SprintSpeed = 10f;
+        public float MoveSpeed = 5f;
+        public float CrouchSpeed = 2f;
 
-    void Update()
-    {
-        Sprint();
-        Crouch();
-    }
+        private float _standHeight = 1.6f;
+        private float _crouchHeight = 1f;
 
-    void Sprint()
-    {
-        if (Input.GetKey(KeyCode.LeftShift) && !_isCrouching && _characterController.isGrounded)
+        private bool _isCrouching;
+
+        private PlayerFootsteps _playerFootsteps;
+        private CharacterController _characterController;
+        private float _sprintVolume = 1f;
+        private float _crouchVolume = 0.1f;
+        private float _walkVolumeMin = 0.2f, _walkVolumeMax = 0.6f;
+        private float _walkStepDistance = 0.4f;
+        private float _sprintStepDistance = 0.25f;
+        private float _crouchStepDistance = 0.5f;
+
+        void Start()
         {
-            _playerMovement.Speed = SprintSpeed;
-            _playerFootsteps.VolumeMin = _sprintVolume;
-            _playerFootsteps.VolumeMax = _sprintVolume;
-            _playerFootsteps.StepDistance = _sprintStepDistance;
-        } else if (!_isCrouching && _characterController.isGrounded)
-        {
-            _playerMovement.Speed = MoveSpeed;
             _playerFootsteps.VolumeMin = _walkVolumeMin;
             _playerFootsteps.VolumeMax = _walkVolumeMax;
             _playerFootsteps.StepDistance = _walkStepDistance;
         }
-    }
 
-    void Crouch()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
+        void Awake()
         {
-            if (_isCrouching)
+            _playerMovement = GetComponent<PlayerMovement>();
+            _lookRoot = transform.GetChild(0);
+            _playerFootsteps = GetComponentInChildren<PlayerFootsteps>();
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        void Update()
+        {
+            Sprint();
+            Crouch();
+        }
+
+        void Sprint()
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && !_isCrouching && _characterController.isGrounded)
             {
-                _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
+                _playerMovement.Speed = SprintSpeed;
+                _playerFootsteps.VolumeMin = _sprintVolume;
+                _playerFootsteps.VolumeMax = _sprintVolume;
+                _playerFootsteps.StepDistance = _sprintStepDistance;
+            } else if (!_isCrouching && _characterController.isGrounded)
+            {
                 _playerMovement.Speed = MoveSpeed;
-                _isCrouching = false;
                 _playerFootsteps.VolumeMin = _walkVolumeMin;
                 _playerFootsteps.VolumeMax = _walkVolumeMax;
                 _playerFootsteps.StepDistance = _walkStepDistance;
             }
-            else
-            {
-                _lookRoot.localPosition = new Vector3(0f, _crouchHeight, 0f);
-                _playerMovement.Speed = CrouchSpeed;
-                _isCrouching = true;
-                _playerFootsteps.VolumeMin = _crouchVolume;
-                _playerFootsteps.VolumeMax = _crouchVolume;
-                _playerFootsteps.StepDistance = _crouchStepDistance;
-
-            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isCrouching)
+        void Crouch()
         {
-            _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
-            _playerMovement.Speed = MoveSpeed;
-            _isCrouching = false;
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                if (_isCrouching)
+                {
+                    _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
+                    _playerMovement.Speed = MoveSpeed;
+                    _isCrouching = false;
+                    _playerFootsteps.VolumeMin = _walkVolumeMin;
+                    _playerFootsteps.VolumeMax = _walkVolumeMax;
+                    _playerFootsteps.StepDistance = _walkStepDistance;
+                }
+                else if (_characterController.isGrounded)
+                {
+                    _lookRoot.localPosition = new Vector3(0f, _crouchHeight, 0f);
+                    _playerMovement.Speed = CrouchSpeed;
+                    _isCrouching = true;
+                    _playerFootsteps.VolumeMin = _crouchVolume;
+                    _playerFootsteps.VolumeMax = _crouchVolume;
+                    _playerFootsteps.StepDistance = _crouchStepDistance;
+
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && _isCrouching)
+            {
+                _lookRoot.localPosition = new Vector3(0f, _standHeight, 0f);
+                _playerMovement.Speed = MoveSpeed;
+                _isCrouching = false;
+            }
         }
     }
 }
