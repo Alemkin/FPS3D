522b683942db268d1158f4cb82968fce6f2165cb
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-06-28 00:23:32 -0400

    Added crosshair, aiming, and aim zoom

--- Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -12,53 +12,99 @@ namespace Assets.Scripts.PlayerScripts
         public float Damage = 20f;
 
         private float _nextTimeToFire;
+        private Animator _zoomCameraAnim;
+        private bool _isZoomed;
 
+        private Camera _mainCamera;
+
+        private GameObject _crossHair;
+
+        private bool _isAiming;
 
         // Start is called before the first frame update
         void Awake()
         {
             _weaponManager = GetComponent<WeaponManager>();
+            _zoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+            _crossHair = GameObject.FindWithTag(Tags.CROSS_HAIR);
+
+            _mainCamera = Camera.main;
         }
 
         // Update is called once per frame
         void Update()
         {
             ShootWeapon();
+            ZoomInAndOut();
         }
 
         void ShootWeapon()
         {
             var currentWeapon = _weaponManager.GetCurrentSelectedWeapon();
-            if (currentWeapon.FireType == WeaponFireType.Multiple)
+            if (currentWeapon.FireType == WeaponFireType.Multiple && Input.GetMouseButton(0) && Time.time > _nextTimeToFire)
             {
-                if (Input.GetMouseButton(0) && Time.time > _nextTimeToFire)
-                {
-                    _nextTimeToFire = Time.time + 1f / FireRate;
+                _nextTimeToFire = Time.time + 1f / FireRate;
 
+                currentWeapon.AttackAnimation();
+
+                //BulletFired();
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                if (currentWeapon.tag == Tags.AXE_TAG)
+                {
+                    currentWeapon.AttackAnimation();
+                }
+
+                if (currentWeapon.BulletType == WeaponBulletType.Bullet)
+                {
                     currentWeapon.AttackAnimation();
 
                     //BulletFired();
                 }
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0))
+                else if (_isAiming)
                 {
-                    if (currentWeapon.tag == Tags.AXE_TAG)
+                    //Arrow or Spear
+                    currentWeapon.AttackAnimation();
+                    switch (currentWeapon.BulletType)
                     {
-                        currentWeapon.AttackAnimation();
+                        case WeaponBulletType.Arrow:
+                            break;
+                        case WeaponBulletType.Spear:
+                            break;
                     }
+                }
+            }
+        }
 
-                    if (currentWeapon.BulletType == WeaponBulletType.Bullet)
-                    {
-                        currentWeapon.AttackAnimation();
+        void ZoomInAndOut()
+        {
+            var currentWeapon = _weaponManager.GetCurrentSelectedWeapon();
+            if (currentWeapon.WeaponAim == WeaponAim.Aim)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    _zoomCameraAnim.Play(AnimationTags.ZOOM_IN);
+                    _crossHair.SetActive(false);
+                }
+                if (Input.GetMouseButtonUp(1))
+                {
+                    _zoomCameraAnim.Play(AnimationTags.ZOOM_OUT);
+                    _crossHair.SetActive(true);
+                }
+            }
 
-                        //BulletFired();
-                    }
-                    else
-                    {
-                        //arrow or spear, must be aiming
-                    }
+            if (currentWeapon.WeaponAim == WeaponAim.SelfAim)
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    currentWeapon.Aim(true);
+                    _isAiming = true;
+                }
+                if (Input.GetMouseButtonUp(1))
+                {
+                    currentWeapon.Aim(false);
+                    _isAiming = false;
                 }
             }
         }
