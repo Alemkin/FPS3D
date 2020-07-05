8c2d916d82c638a364af6d7afc1f0bee6a527c53
alemkin@netchexonline.com <alemkin@netchexonline.com>
2020-07-05 15:10:33 -0400

    Added enemy and aniomationanimations plus base class

---
+++ Assets/Scripts/Enemy Scripts/EnemyAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemy_Scripts
+{
+    public class EnemyAnimator : MonoBehaviour
+    {
+
+        private Animator _animator;
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Walk(bool walk)
+        {
+            _animator.SetBool(AnimationTags.WALK_PARAMETER, walk);
+        }
+
+        public void Run(bool run)
+        {
+            _animator.SetBool(AnimationTags.RUN_PARAMETER, run);
+        }
+
+        public void Attack()
+        {
+            _animator.SetTrigger(AnimationTags.ATTACK_TRIGGER);
+        }
+
+        public void Dead()
+        {
+            _animator.SetTrigger(AnimationTags.DEAD_TRIGGER);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Enemy_Scripts
+{
+    public class EnemyController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
